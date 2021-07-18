f4a1519476632e7a1b956499ea60ec5027d5d6c9
asads30 <itmasteruz@yandex.com>
2021-07-18 23:34:33 +0500

    six

--- src/index.js
+++ src/index.js
@@ -543,7 +543,9 @@ bot.on("message", (msg) => {
                      for (var key in result) {
                         bot.sendPhoto(
                            admin,
-                           `https://api.telegram.org/file/bot${config.TOKEN}/${msg.photo[0].file_id}`,
+                           `https://api.telegram.org/file/bot${
+                              config.TOKEN
+                           }/${JSON.parse(msg.photo[0].file_id)}`,
                            {
                               caption: msg.text,
                               parse_mode: "HTML",
